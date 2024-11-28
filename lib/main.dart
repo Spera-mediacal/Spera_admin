37fbb5546f90019f2fefd2ac6db512b951cef44f
mahmodhafez <mahmodahmdhafez@gmail.com>
2024-11-28 15:30:44 +0200

    Add min window size constraint for desktop application

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:bitsdojo_window/bitsdojo_window.dart'; // مكتبة التحكم في النافذة
+import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:spera_admin_panel/app/routes/app_routes.dart';
 import 'package:spera_admin_panel/utils/colors.dart';
 
@@ -35,37 +35,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-/*
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:spera_admin_panel/app/routes/app_routes.dart';
-import 'package:spera_admin_panel/utils/colors.dart';
-
-void main() {
-  runApp(const MyApp());
-
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: AppColors.accentColor),
-        useMaterial3: true,
-        scaffoldBackgroundColor: AppColors.bgColor,
-        fontFamily:'Manjari'
-      ),
-      initialRoute: AppRoutes.loginViewPath,
-
-      getPages: AppRoutes.getRoutes(),
-    );
-  }
-}
-
-*/
